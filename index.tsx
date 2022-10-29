fd50b1b30aa9c21a3008411175b8a31ba7d913fd
Colby Birkhead <birkheadc@gmail.com>
2022-10-29 20:17:01 +0900

    first commit

---
+++ index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import './index.css';
+import './vars.css';
+
+import App from './src/App';
+
+
+const container = document.getElementById('root');
+if (container != null) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css'
+
+interface IAppProps {
+
+}
+
+function App(props: IAppProps): JSX.Element  {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
